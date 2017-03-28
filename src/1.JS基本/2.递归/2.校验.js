b4bf67ab9ff088c40583e3cd807f79af0ea76b44
李宏吉 <m18513622209@163.com>
2017-03-28 22:22:21 +0800

    add

--- src/1.JS基本/2.递归/2.校验.js
+++ src/1.JS基本/2.递归/2.校验.js
@@ -1,25 +1,51 @@
-const fs = require('fs');
-const config = require('./data');
+const config = require('./data')
 
-const checkTreeLeaf = rule => {
-  let memoryCache = [];
+const checkTreeLeaf = (rule) => {
+  const memoryCache = []
+  const bindsCache = []
   const renderTree = (leaf, path) => {
-    leaf.children && leaf.children.map((subLeaf, i)=> {
-      const subPath = [path, i].join('.');
+    /* eslint-disable no-unused-expressions */
+    leaf && leaf.children && leaf.children.map((subLeaf, i) => {
+      const subPath = [path, i].join('.')
       return renderTree(leaf.children[i], subPath)
-    });
+    })
 
-    if (!rule(leaf)) {
-      memoryCache.push(path);
+    const { success, message } = rule(leaf, path)(memoryCache, bindsCache)
+    if (!success) {
+      memoryCache.push(message)
     }
     return memoryCache
-  };
-  return renderTree;
-};
+  }
+  return renderTree
+}
 
 // 校验bind值必须是string且不能为空字符串
-const rule = leaf => leaf.bind === undefined || typeof leaf.bind === 'string' && leaf.bind.length !== 0;
+const rule = (leaf, path) => (memoryCache, bindsCache) => {
+  if (!leaf) {
+    return {
+      success: false,
+      level: 'warning',
+      message: `页面组件编译有误，保存后可能会无法打开页面，请检查组件配置，组件路径${path}`,
+    }
+  }
 
-console.log(checkTreeLeaf(rule)(config, 'root'));
+  if (bindsCache.find(bind => bind.bind === leaf.bind) !== undefined) {
+    return {
+      success: false,
+      level: 'error',
+      message: `bind重复, 路径${bindsCache.find(bind => bind.bind === leaf.bind).path} and ${path}`,
+    }
+  }
 
+  if (leaf.bind !== undefined) {
+    bindsCache.push({
+      bind: leaf.bind,
+      path,
+    })
+  }
+  return {
+    success: true,
+  }
+}
 
+console.log(checkTreeLeaf(rule)(config, 'root'))
