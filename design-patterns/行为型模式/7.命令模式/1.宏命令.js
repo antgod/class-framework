5217593ab262c950c15cf47616396591048b0ac6
antgod <m18513622209@163.com>
2019-01-20 23:56:54 +0800

    fix: 命令模式

---
+++ design-patterns/行为型模式/7.命令模式/1.宏命令.js
@@ -0,0 +1,28 @@
+const openComputer = {
+	execute: function () {
+		console.log("打开电脑");
+	}
+};
+
+const openQQ = {
+	execute: function () {
+		console.log("打开QQ");
+	}
+};
+
+const MacroCommand = function () {
+	return {
+		commandList: [],
+		add: function (command) {
+			this.commandList.push(command);
+		},
+		execute: function () {
+			this.commandList.forEach(command => command.execute())
+		}
+	}
+};
+const macroCommand = MacroCommand();
+macroCommand.add(openComputer);
+macroCommand.add(openQQ);
+
+macroCommand.execute();
